ea3e48d45ddfd52f63d445d570056aabafe07d93
sharan-wappnet <156918025+sharan-wappnet@users.noreply.github.com>
2024-02-05 17:19:00 +0530

    Adding new file

---
+++ Practice-1.php
@@ -0,0 +1,30 @@
+<?php
+
+/*Variables
+  -Containers for Storing information 
+  - PHP supports the following data types:
+        String
+        Integer
+        Float (floating point numbers - also called double)
+        Boolean
+        Array
+        Object
+        NULL
+        Resource
+
+*/
+
+// Example:-
+echo $string = "hello ";  //String
+echo $num = 1;  //Intege
+echo $float = 2.444; //Float
+echo $boolean = true; //Boolean
+echo $null = null; //null value
+
+//array has 3 types of values (use var_dump to export array like variables)
+// 1. Indexed array 
+ var_dump($indexedarray = ["a", "b", "c", "d"]);
+
+//2. Associative array
+ var_dump($)
+?>
